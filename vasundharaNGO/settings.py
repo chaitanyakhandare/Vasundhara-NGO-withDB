322f9686bca49047bd01e558697e9c923551e674
Chaitanya Khandare <115148924+chaitanyakhandare@users.noreply.github.com>
2023-11-10 02:01:07 +0530

    Update settings.py

--- vasundharaNGO/settings.py
+++ vasundharaNGO/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'vasundharaNGO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.pysqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
